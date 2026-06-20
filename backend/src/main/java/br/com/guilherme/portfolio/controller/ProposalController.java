5d3fdebdccffdfe25435152e1ed4f38170fb27f2
user <guilhermevasconcelosdev@gmail.com>
2026-06-19 23:18:55 -0300

    feat: a bixo, aguento mais

--- backend/src/main/java/br/com/guilherme/portfolio/controller/ProposalController.java
+++ backend/src/main/java/br/com/guilherme/portfolio/controller/ProposalController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.guilherme.portfolio.entity.RecruiterProposal;
@@ -17,7 +16,7 @@ import br.com.guilherme.portfolio.repository.RecruiterProposalRepository;
 
 @RestController
 @RequestMapping("/api/propostas")
-@CrossOrigin(origins = "${app.frontend.url}", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
+@CrossOrigin(origins = "${app.frontend.url}")
 public class ProposalController {
 
     @Autowired
